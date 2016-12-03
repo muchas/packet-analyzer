82268b5cc802b0095757b94df957fab91ab7c67e
Marcin Lasoń <marcin.lason1@gmail.com>
2016-12-03 19:41:40 +0100

    Improve launching charts

--- src/main/java/pl/edu/agh/iisg/to/visualizer/demo/DemoStarter.java
+++ src/main/java/pl/edu/agh/iisg/to/visualizer/demo/DemoStarter.java
@@ -7,9 +7,20 @@ public class DemoStarter {
 
     public static void main(String[] args) throws Exception {
 
-        LiveLineChart.launch(LiveLineChart.class);
+        new Thread() {
+            @Override
+            public void run() {
+                javafx.application.Application.launch(PieChartSample.class);
+            }
+        }.start();
+
+        PieChartSample pieChartSample = PieChartSample.waitPieChartSample();
+        pieChartSample.printSomething();
+        pieChartSample.setAmountOfTCP(2000);
+
         //PieChartSample.launch(PieChartSample.class);
         //BarChartSample.launch(BarChartSample.class);
+        //LiveLineChart.launch(LiveLineChart.class);
 
     }
 }
--- src/main/java/pl/edu/agh/iisg/to/visualizer/demo/PieChartSample.java
+++ src/main/java/pl/edu/agh/iisg/to/visualizer/demo/PieChartSample.java
@@ -1,16 +1,61 @@
-package main.java.pl.edu.agh.iisg.to.visualizer.demo; /**
+package main.java.pl.edu.agh.iisg.to.visualizer.demo;
+/**
  * Created by Cinek on 2016-11-29.
  */
-
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.chart.*;
 import javafx.stage.Stage;
+import javafx.scene.chart.*;
+import javafx.scene.Group;
+
+import java.util.concurrent.CountDownLatch;
 
 public class PieChartSample extends Application {
+    public static final CountDownLatch latch = new CountDownLatch(1);
+    public static PieChartSample pieChartSample = null;
+    private int amountOfTCP = 432;
+    private int amountOfUDP = 256;
+
+    ObservableList<PieChart.Data> pieChartData =
+            FXCollections.observableArrayList(
+                    new PieChart.Data("TCP", amountOfTCP),
+                    new PieChart.Data("UDP", amountOfUDP));
+
+    public void setAmountOfTCP(int amount) {
+        this.amountOfTCP = amount;
+        pieChartData.remove("TCP");
+        pieChartData.add(new PieChart.Data("TCP", amount));
+    }
+
+    public void setAmountOfUDP(int amount) {
+        this.amountOfUDP = amount;
+    }
+
+    public static PieChartSample waitPieChartSample() {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return pieChartSample;
+    }
+
+    public static void setPieChartSample(PieChartSample pieChartSample0) {
+        pieChartSample = pieChartSample0;
+        latch.countDown();
+    }
+
+    public PieChartSample() {
+        setPieChartSample(this);
+    }
+
+    public void printSomething() {
+        System.out.println("You called a method on the application");
+    }
+
+
 
     @Override public void start(Stage stage) {
         Scene scene = new Scene(new Group());
@@ -18,15 +63,16 @@ public class PieChartSample extends Application {
         stage.setWidth(500);
         stage.setHeight(500);
 
-        ObservableList<PieChart.Data> pieChartData =
-                FXCollections.observableArrayList(
-                        new PieChart.Data("TCP", 432),
-                        new PieChart.Data("UDP", 256));
+
         final PieChart chart = new PieChart(pieChartData);
         chart.setTitle("Transport Layer Protocols Usage");
 
         ((Group) scene.getRoot()).getChildren().add(chart);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
