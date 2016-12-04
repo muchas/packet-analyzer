73710b113beba3dadf68f965893842bc18977d64
Marcin Lasoń <marcin.lason1@gmail.com>
2016-12-04 22:19:08 +0100

    Fix auto-ranging in LiveLineChart

--- src/main/java/pl/edu/agh/iisg/to/visualizer/demo/LiveLineChart.java
+++ src/main/java/pl/edu/agh/iisg/to/visualizer/demo/LiveLineChart.java
@@ -15,15 +15,13 @@ import java.util.Random;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class LiveLineChart extends Application {
-
     private int xSeriesData = 0;
-    private int MAX_DATA_POINTS = 50;
+    private int MAX_DATA_POINTS = 100;
     private XYChart.Series series;
     private ExecutorService executor;
     private AddToQueue addToQueue;
@@ -31,66 +29,48 @@ public class LiveLineChart extends Application {
 
     private NumberAxis xAxis = new NumberAxis();
     final NumberAxis yAxis = new NumberAxis();
-    final LineChart<Number,Number> lineChart = new LineChart<Number,Number>(xAxis,yAxis);
+    final LineChart<Number, Number> lineChart = new LineChart<Number, Number>(xAxis, yAxis);
 
     private void init(Stage primaryStage) {
-        xAxis = new NumberAxis();
         xAxis.setForceZeroInRange(false);
-        xAxis.setAutoRanging(true);
+        xAxis.setAutoRanging(false);
         xAxis.setTickLabelsVisible(true);
         xAxis.setTickMarkVisible(true);
         xAxis.setMinorTickVisible(true);
-
-
-        NumberAxis yAxis = new NumberAxis();
         yAxis.setAutoRanging(true);
-
-        final LineChart<Number, Number> sc = new LineChart<Number, Number>(xAxis, yAxis) {
-            @Override protected void dataItemAdded(Series<Number, Number> series, int itemIndex, Data<Number, Number> item) {}
-        };
-        sc.setAnimated(true);
-        sc.setId("liveLineChart");
-        sc.setTitle("Animated Line Chart");
 
         series = new XYChart.Series<Number, Number>();
         lineChart.getData().addAll(series);
-        lineChart.setMouseTransparent(true);
-
-
         series.setName("Packet Size");
         primaryStage.setScene(new Scene(lineChart));
     }
 
 
-    @Override public void start(Stage stage) {
+    @Override
+    public void start(Stage stage) {
         stage.setTitle("Animated Line Chart Sample");
         stage.setWidth(1500);
         init(stage);
         stage.show();
 
-
-        executor = Executors.newCachedThreadPool(new ThreadFactory() {
-            @Override public Thread newThread(Runnable r) {
-                Thread thread = new Thread(r);
-                thread.setDaemon(true);
-                return thread;
-            }
+        executor = Executors.newCachedThreadPool(r -> {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            return thread;
         });
         addToQueue = new AddToQueue();
         executor.execute(addToQueue);
-        //-- Prepare Timeline
         prepareTimeline();
-
-
     }
 
     private class AddToQueue implements Runnable {
         Random random = new Random();
+
         @Override
         public void run() {
             try {
                 data.add(random.nextInt(1518));
-                Thread.sleep(300);
+                Thread.sleep(random.nextInt(300));
                 executor.execute(this);
             } catch (InterruptedException ex) {
                 Logger.getLogger(LiveLineChart.class.getName()).log(Level.SEVERE, null, ex);
@@ -100,7 +80,8 @@ public class LiveLineChart extends Application {
 
     private void prepareTimeline() {
         new AnimationTimer() {
-            @Override public void handle(long now) {
+            @Override
+            public void handle(long now) {
                 addDataToSeries();
             }
         }.start();
@@ -112,14 +93,8 @@ public class LiveLineChart extends Application {
             series.getData().add(new AreaChart.Data(xSeriesData++, data.remove()));
         }
 
-        int amountOfDataToRemove = 0;
-        if (series.getData().size() > MAX_DATA_POINTS) {
-            amountOfDataToRemove = series.getData().size() - MAX_DATA_POINTS;
-            series.getData().remove(0, amountOfDataToRemove);
-        }
-
-        xAxis.setTranslateX(amountOfDataToRemove);
-        xAxis.setLowerBound(xSeriesData - MAX_DATA_POINTS);
-        xAxis.setUpperBound(xSeriesData-1);
+        xAxis.setLowerBound(xSeriesData - MAX_DATA_POINTS > 0 ? xSeriesData - MAX_DATA_POINTS : 0);
+        xAxis.setUpperBound(xSeriesData - 1 > MAX_DATA_POINTS ? xSeriesData - 1 : MAX_DATA_POINTS);
+        System.out.println(series.getData().size());
     }
 }
