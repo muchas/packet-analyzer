7ce6b01d20acf10cd57a5828a255ba9b67b29baa
Suota <slota.olga@gmail.com>
2016-12-13 20:39:09 +0100

    ok

--- src/main/java/pl/edu/agh/iisg/to/visualizer/demo/DemoStarter.java
+++ src/main/java/pl/edu/agh/iisg/to/visualizer/demo/DemoStarter.java
@@ -7,20 +7,20 @@ public class DemoStarter {
 
     public static void main(String[] args) throws Exception {
 
-        new Thread() {
-            @Override
-            public void run() {
-                javafx.application.Application.launch(PieChartSample.class);
-            }
-        }.start();
+//        new Thread() {
+//            @Override
+//            public void run() {
+//                javafx.application.Application.launch(PieChartSample.class);
+//            }
+//        }.start();
 
-        PieChartSample pieChartSample = PieChartSample.waitPieChartSample();
-        pieChartSample.printSomething();
-        pieChartSample.setAmountOfTCP(2000);
+//        PieChartSample pieChartSample = PieChartSample.waitPieChartSample();
+//        pieChartSample.printSomething();
+//        pieChartSample.setAmountOfTCP(2000);
 
-        //PieChartSample.launch(PieChartSample.class);
-        //BarChartSample.launch(BarChartSample.class);
-        //LiveLineChart.launch(LiveLineChart.class);
+//        PieChartSample.launch(PieChartSample.class);
+//        BarChartSample.launch(BarChartSample.class);
+        LiveLineChart.launch(LiveLineChart.class);
 
     }
 }
