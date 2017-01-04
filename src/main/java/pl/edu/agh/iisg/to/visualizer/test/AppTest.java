7edac71882f12b643a62ba5c1c3b60b3420917d0
Marcin Lasoń <marcin.lason1@gmail.com>
2017-01-04 23:33:28 +0100

    Merge test classes into AppTest, refactore tests code

--- src/main/java/pl/edu/agh/iisg/to/visualizer/test/AppTest.java
+++ src/main/java/pl/edu/agh/iisg/to/visualizer/test/AppTest.java
@@ -3,6 +3,7 @@ package main.java.pl.edu.agh.iisg.to.visualizer.test;
 import com.athaydes.automaton.FXApp;
 import com.athaydes.automaton.FXer;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import main.java.pl.edu.agh.iisg.to.visualizer.src.App;
 import org.junit.AfterClass;
@@ -16,29 +17,34 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * Created by Suota on 2017-01-03.
  */
-public class SampleTest {
+public class AppTest {
 
     @BeforeClass
-    public static void launchApp() throws Exception {
+    public static void setUp() throws Exception {
         System.out.println( "Launching Java App" );
-
         FXApp.startApp( new App() );
 
-
         System.out.println( "App has been launched" );
-
         // let the window open and show before running tests
         Thread.sleep( 2000 );
     }
 
     @AfterClass
-    public static void cleanup() {
+    public static void cleanUp() {
         System.out.println( "Cleaning up" );
         FXApp.doInFXThreadBlocking( () -> FXApp.getStage().close() );
     }
 
     @Test
-    public void automatonTest() {
+    public void shouldFindPacketAnalyzerLabelOnStartTest() {
+        System.out.println( "Running test" );
+        FXer user = FXer.getUserWith();
+
+        assertThat( user.getAt(Label.class),hasText( "Welcome to Packet Analyzer Tool" ) );
+    }
+
+    @Test
+    public void shouldDisplayTextOfUserGuideTest() {
         System.out.println( "Running test" );
         FXer user = FXer.getUserWith();
 
@@ -48,7 +54,6 @@ public class SampleTest {
         user.clickOn(matchingAny("type:Button", "text:User Guide") );
         assertThat( user.getAt(TextArea.class),
                 hasText( "This is Sample User Guide.\n\nGot few lines....\n\n... but nothing worth seeing yet.\n" ) );
-
     }
 }
 
