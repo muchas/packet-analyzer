f17bcf4cf456852f2af27e03fcec353fbe013785
Maciej Rapacz <maciek.rapacz@gmail.com>
2017-01-11 23:10:58 +0100

    Add first tests

--- src/sample/FilterTest.java
+++ src/sample/FilterTest.java
@@ -1,6 +1,7 @@
 package sample;
 
 import org.junit.Test;
+import sample.entities.Filter;
 
 import static org.junit.Assert.*;
 
@@ -8,22 +9,24 @@ public class FilterTest {
 
     @Test
     public void testGetCode() throws Exception {
-
-    }
-
-    @Test
-    public void testSetCode() throws Exception {
-
+        Filter filter = new Filter("name", "code");
+        assertEquals("function name(packet) { \n" +
+                "code\n" +
+                "}", filter.getCode());
     }
 
     @Test
     public void testGetName() throws Exception {
-
+        Filter filter = new Filter("name", "code");
+        assertEquals("name", filter.getName());
     }
 
     @Test
     public void testSetName() throws Exception {
+        Filter filter = new Filter("name", "code");
 
+        filter.setName("differentname");
+        assertEquals("differentname", filter.getName());
     }
 
     @Test
